d410b4911cf9c0e25749525a5f9ada969fe66e85
Ashwin Nyamainashe <104971785+Ashwin2926@users.noreply.github.com>
2025-07-05 15:09:17 +0400

    fx

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-    domains: ['unsplash.com'], // Add 'placehold.co' here
+  images: {
+    domains: ['unsplash.com', 'placehold.co'],
   },
 };
 
